ae1cd63d197ce0a88c7795d9b2375a36bfc0bfb5
Wacław Łuczak <waclaw@luczak.it>
2012-09-25 00:16:59 +0200

    added newest version of xmcda in writeHeader funcion

--- PyXMCDA.py
+++ PyXMCDA.py
@@ -978,7 +978,7 @@ def xmlWrite (xmltree, xmlFileName):
 
 def writeHeader (xmlfile) :
 	xmlfile.write ("<?xml version='1.0' encoding='UTF-8'?>\n<?xml-stylesheet type='text/xsl' href='xmcdaXSL.xsl'?>\n")
-	xmlfile.write("<xmcda:XMCDA xmlns:xmcda='http://www.decision-deck.org/2009/XMCDA-2.0.0' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.decision-deck.org/2009/XMCDA-2.0.0 http://sma.uni.lu/d2cms/xmcda/_downloads/XMCDA-2.0.0.xsd'>\n\n")
+	xmlfile.write("<xmcda:XMCDA xmlns:xmcda='http://www.decision-deck.org/2012/XMCDA-2.2.1' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.decision-deck.org/2012/XMCDA-2.2.1 http://www.decision-deck.org/xmcda/_downloads/XMCDA-2.2.1.xsd'>\n\n")
 
 
 ##########
